ab22d9e8da7f1004e59edabec243c00c0711d153
xyj5094 <143439554+XinYuJ20@users.noreply.github.com>
2024-09-28 13:57:44 -0400

    button
    - added a non working button

---
+++ Assets/button.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour
+{
+    
+     public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Character")
+        {
+        
+            ButtonPressed();
+        }
+    }
+
+    void ButtonPressed()
+    {
+        Debug.Log("Button Pressed");
+    }
+}
